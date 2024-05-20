a1ebb9992839e89275168d8bf0fca0165e4a39e6
Athira K S <athiraks@Athiras-MacBook-Air.local>
2024-05-20 12:01:16 +0530

    column component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Column from "./column";
+import sedans from "./assets/images/icon-sedans.svg";
+import suvs from "./assets/images/icon-suvs.svg";
+import luxury from "./assets/images/icon-luxury.svg";
 
 function App() {
+  const columnDetails = [
+    {
+      logo: sedans,
+      title: "Sedans",
+      description:
+        "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    },
+    {
+      logo: suvs,
+      title: "SUVs",
+      description:
+        "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+    },
+    {
+      logo: luxury,
+      title: "Luxury",
+      description:
+        "  Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+    },
+  ];
+  const listItems = columnDetails.map((col) => (
+    <Column
+      logo={col.logo}
+      title={col.title}
+      description={col.description}
+    ></Column>
+  ));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="three-column-container">{listItems}</div>
     </div>
   );
 }
---
+++ src/column.jsx
@@ -0,0 +1,8 @@
+
+function Column(logo,title,description){
+    return(
+        <div className="column-container"></div>
+    )
+}
+
+export default Column;
